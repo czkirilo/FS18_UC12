0174af15b860b7d727e52199315a60a4cd192502
Caique Zaneti Kirilo <caiquez.kirilo@hotmail.com>
2023-05-08 19:32:00 -0300

    ER7

--- Sistema_UC12/Program.cs
+++ Sistema_UC12/Program.cs
@@ -34,6 +34,7 @@ static void Main(string[] args)
 
 BarraCarregamento("INICIANDO");
 
+List<PessoaFisica> listaPf = new List<PessoaFisica>();
 Console.Clear();
 
  string opcao;
@@ -57,30 +58,108 @@ do {
 
   opcao = Console.ReadLine();
 
+
+
  switch (opcao)
  {
     case "1":
-    Endereco enderecodoygor = new Endereco();
-    enderecodoygor.logradouro = "Rua Niteroi";
-    enderecodoygor.numero = 180;
-    enderecodoygor.complemento = "Escola Senai Paulo Skaf";
-    enderecodoygor.enderecoComercial = true;
+string? opcaoPF;
+do{
+Console.Clear();
+Console.WriteLine(@$"
 
+    ♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡
+    ♡                                                                       ♡  
+    ♡                     Escolha uma das opções abaixo                     ♡
+    ♡                                                                       ♡
+    ♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡
+    ♡                                                                       ♡  
+    ♡                       1 - Cadastrar Pessoa Física                     ♡
+    ♡                       2 - Mostrar Pessoas Fisicas                     ♡
+    ♡                                                                       ♡
+    ♡                       0 - Voltar ao menu anterior                     ♡
+    ♡                                                                       ♡
+    ♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡
+    
+ ");
+ opcaoPF = Console.ReadLine();
+
+switch (opcaoPF){
+    case "1":
     PessoaFisica novapf = new PessoaFisica();
+    Endereco endereco = new Endereco();
 
-    novapf.cpf = "4555555555";
-    novapf.endereco = enderecodoygor;
-    novapf.dataNascimento = new DateTime (2004, 01, 18);
-    novapf.nome = "Ygor de Andrade";
+    Console.WriteLine("Digite o logradouro da pessoa física que deseja cadastrar");
+    endereco.logradouro = Console.ReadLine();
 
-    bool idadevalidada = novapf.ValidarDataNascimento(novapf.dataNascimento);
-if (idadevalidada == true){
-    Console.WriteLine($"Pode beber pinga");
-}else{
-    Console.WriteLine($"Vai ficar só no refri");
+    Console.WriteLine("Digite o numero");
+    endereco.numero = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o complemento");
+    endereco.complemento = Console.ReadLine();
+    
+    Console.WriteLine("Se trata de um local comercial? S/N");
+    string endCom = Console.ReadLine().ToUpper();
+
+    if (endCom == "S"){
+        endereco.enderecoComercial = true;
+    }
+    else{
+        endereco.enderecoComercial = false;
+    }
+
+    Console.WriteLine("Digite o cpf");
+    novapf.cpf = Console.ReadLine();
+
+    novapf.endereco = endereco;
+
+     Console.WriteLine("Digite A data de nascimento Ex: DD/MM/AAAA");
+     novapf.dataNascimento = DateTime.Parse(Console.ReadLine());
+
+          bool idadevalidada = novapf.ValidarDataNascimento(novapf.dataNascimento);
+ if (idadevalidada == true){
+     Console.WriteLine($"Pode beber pinga");
+ }else{
+     Console.WriteLine($"Vai ficar só no refri");
+ }
+
+      Console.WriteLine("Digite o nome da pessoa física que deseja cadastrar");
+      novapf.nome = Console.ReadLine();
+
+      listaPf.Add(novapf);
+      Console.WriteLine("cadastro efetuado com sucesso");
+      Thread.Sleep(4000);
+        break;
+    case "2":
+    Console.Clear();
+    if (listaPf.Count > 0){
+        foreach(PessoaFisica cadaPessoa in listaPf){
+            Console.Clear();
+            Console.WriteLine(@$"
+            Nome: {cadaPessoa.nome}
+            Endereco: {cadaPessoa.endereco.logradouro}, {cadaPessoa.endereco.numero}
+            Data de Nascimento: {cadaPessoa.dataNascimento}
+            ");
+        Console.WriteLine("Digite qualquer tecla para continuar");
+        Console.ReadLine();
+        }
+    }else{
+        Console.WriteLine("Lista Vazia!!! Digite qualquer tecla para continuar");
+        Console.ReadLine();
+    }
+        break;
+    case "0":
+        break;
+    default:
+    Console.Clear();
+    Console.WriteLine("Opção Inválida, por favor digite outra opção");
+    Thread.Sleep(2000);
+        break;
 }
 
-    Console.WriteLine($"O endereço do {novapf.nome} é {novapf.endereco.logradouro}, numero {novapf.endereco.numero}");
+}while (opcaoPF != "0");
+
+//     Console.WriteLine($"O endereço do {novapf.nome} é {novapf.endereco.logradouro}, numero {novapf.endereco.numero}");
 
     break;
     case "2":
